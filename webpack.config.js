2db430195ee63ff9498a9f65cddd6dacede7062c
Sergey Milaev <milaev@ksolutions.com.ua>
2020-04-07 12:53:06 +0300

    change project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const folderPath = "./InvolveServiceEcard";
+const folderPath = './LayoutQWallety';
 
 module.exports = {
-  entry: folderPath + "/app/App.js",
+  entry: folderPath + '/app/App.js',
   output: {
-    path: path.resolve(__dirname, folderPath + "/public"),
-    filename: "js/bundle.js"
+    path: path.resolve(__dirname, folderPath + '/public'),
+    filename: 'js/bundle.js',
   },
   devServer: {
     historyApiFallback: true,
     contentBase: path.join(__dirname, folderPath),
-    port: 8081
+    port: 8081,
     // host: '192.168.1.140'
     // host: '192.168.1.140' - параметр для доступа по локальной сети
   },
@@ -24,121 +24,121 @@ module.exports = {
       {
         test: /\.js[x]?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env"]
-        }
+          presets: ['@babel/preset-env'],
+        },
       },
       {
         test: /\.(css|sass|scss)$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: "../"
-            }
+              publicPath: '../',
+            },
           },
-          "css-loader",
-          "postcss-loader",
-          "resolve-url-loader",
+          'css-loader',
+          'postcss-loader',
+          'resolve-url-loader',
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
-              sourceMap: true
-            }
-          }
-        ]
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(jpe?g|png|gif)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "images/"
-            }
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
           },
 
           {
-            loader: "image-webpack-loader"
-          }
-        ]
+            loader: 'image-webpack-loader',
+          },
+        ],
       },
       {
         test: /\.(ico)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "images/"
-            }
-          }
-        ]
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/"
-            }
-          }
-        ]
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
       },
       {
         test: /\.svg$/,
         use: [
           {
-            loader: "svg-sprite-loader",
+            loader: 'svg-sprite-loader',
             options: {
               extract: true,
-              spriteFilename: "images/icons-sprite.svg",
-              publicPath: "images/svg-icons/"
-            }
+              spriteFilename: 'images/icons-sprite.svg',
+              publicPath: 'images/svg-icons/',
+            },
           },
           {
-            loader: "svgo-loader"
+            loader: 'svgo-loader',
           },
           {
-            loader: "image-webpack-loader"
-          }
-        ]
+            loader: 'image-webpack-loader',
+          },
+        ],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: {
               minimize: true,
-              interpolate: true
-            }
-          }
-        ]
-      }
-    ]
+              interpolate: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new SpriteLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename: "css/[name].css"
+      filename: 'css/[name].css',
     }),
     new HtmlWebpackPlugin({
-      template: folderPath + "/app/index.html",
+      template: folderPath + '/app/index.html',
       minify: {
         collapseWhitespace: true,
         removeComments: true,
         removeRedundantAttributes: true,
         removeScriptTypeAttributes: true,
         removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true
-      }
-    })
-  ]
+        useShortDoctype: true,
+      },
+    }),
+  ],
 };
